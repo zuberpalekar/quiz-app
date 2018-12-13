e4a27eba7b82e5af03b99bc4da114cf525587ce2
zuberpalekar <zuber.palekar@in.musafir.com>
2018-12-13 19:49:07 +0530

    Updated the prizes

--- src/reducers/game-stage.reducer.js
+++ src/reducers/game-stage.reducer.js
@@ -1,7 +1,7 @@
 import * as types from './../actions/types';
 
 const initialState = {
-	price_list : [1000, 800, 600, 400, 200, 100, 75, 40, 20, 10],
+	price_list : [1000, 600, 400, 250, 150, 100, 75, 40, 20, 10],
 	won : 0
 };
 
@@ -11,11 +11,11 @@ let price_levels = {
 	"3": 40,
 	"4": 75,
 	"5": 100,
-	"6": 200,
-	"7": 400,
-	"8": 600,
-	"9": 800,
-	"10": 10000
+	"6": 150,
+	"7": 250,
+	"8": 400,
+	"9": 600,
+	"10": 1000
 };
 
 export default (state = initialState, action) => {
